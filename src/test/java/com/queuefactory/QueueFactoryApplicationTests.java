870fa01115aaf12c5def5f2727d9fe5653c991e0
Marcello Andrade <marcellohfandrade@gmail.com>
2020-03-19 13:56:25 -0300

    inicialização do projeto

---
+++ src/test/java/com/queuefactory/QueueFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queuefactory;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class QueueFactoryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
